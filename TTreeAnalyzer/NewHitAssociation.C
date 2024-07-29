2d08beebc804c4dc63d0921bd4c73b95aa1a56d4
Iurii Mitrankov <mitrankovy@gmail.com>
2024-07-28 22:46:20 -0400

    new QA in data

--- TTreeAnalyzer/NewHitAssociation.C
+++ TTreeAnalyzer/NewHitAssociation.C
@@ -10,9 +10,9 @@ void NewHitAssociation(const char* inFile0, const char* outFile, int par = 0)
     cout << "no input file" << endl;
     return;
   }
-  const int remove_hadron_hits = 1;
-  const int fill_QA_hadron_hists = 1;
-  const int fill_QA_lepton_hists = 1;
+  const int remove_hadron_hits = 0;
+  const int fill_QA_hadron_hists = 0;
+  const int fill_QA_lepton_hists = 0;
   const int fill_TTree = 0;
   const int fill_d_dphi_hists = 0;
   const int fill_DCA_hists = 0;
@@ -20,11 +20,11 @@ void NewHitAssociation(const char* inFile0, const char* outFile, int par = 0)
   const int do_track_QA = 0;
   const int do_reveal_hadron = 0;
   const int Use_ident = 0;
-  const int fill_true_DCA = 1;
-  const int check_veto = 1;
+  const int fill_true_DCA = 0;
+  const int check_veto = 0;
   const int is_only_conv = 0;
-  const int fill_inv_mass = 0;
-  const int use_only_vertex_hadrons = 1;
+  const int fill_inv_mass = 1;
+  const int use_only_vertex_hadrons = 0;
 
   TTree *T = (TTree *)input->Get("tree");
   TBranch *br = T->GetBranch("MyEvent");
@@ -113,33 +113,85 @@ void NewHitAssociation(const char* inFile0, const char* outFile, int par = 0)
     
     event_container->Associate_Hits_to_Leptons();
 
-    if (check_veto)
+    if (check_veto||fill_inv_mass)
       event_container->CheckVeto();
+    if(do_track_QA) 
+      event_container->FillQAHist();
     if (fill_true_DCA)
       event_container->FillTrueDCA();
 
 
     n_electrons = event->GetNtrack();
-        for (int itrk = 0; itrk < n_electrons; itrk++)
-        {
-            MyDileptonAnalysis::MyElectron mytrk = *event->GetEntry(itrk);
-            bool do_reshuf = false;
+    for (int itrk = 0; itrk < n_electrons; itrk++)
+    {
+      MyDileptonAnalysis::MyElectron mytrk = *event->GetEntry(itrk);
+      bool do_reshuf = false;
 
-            if (mytrk.GetHitCounter(0) < 1 || mytrk.GetHitCounter(1) < 1 ||
-                (mytrk.GetHitCounter(2) < 1 && mytrk.GetHitCounter(3) < 1))
-                do_reshuf = true;
-            if(TMath::Abs(mytrk.GetDCA2())>50||mytrk.GetGhost()>0) do_reshuf = true;
+      if (mytrk.GetHitCounter(0) < 1 || mytrk.GetHitCounter(1) < 1 ||
+          (mytrk.GetHitCounter(2) < 1 && mytrk.GetHitCounter(3) < 1))
+          do_reshuf = true;
+      if((TMath::Abs(mytrk.GetDCA2())>50||mytrk.GetGhost()>0)&&use_only_vertex_hadrons) do_reshuf = true;
 
-            if (do_reshuf)
-            {
-                event->RemoveHadronEntry(itrk);
-                event->RemoveTrackEntry(itrk);
-                //event->AddElecCand(&mytrk);
-                n_electrons--;
-                itrk--;
-                continue;
-            }
-        }
+      if (do_reshuf)
+      {
+          if(use_only_vertex_hadrons) event->RemoveHadronEntry(itrk);
+          event->RemoveTrackEntry(itrk);
+          //event->AddElecCand(&mytrk);
+          n_electrons--;
+          itrk--;
+          continue;
+      }
+      if(fill_inv_mass && event->GetCentrality()<60)
+      {
+        bool skip = false; 
+        if (mytrk.GetPtPrime()<0.3 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=6 + mytrk.GetDisp()*mytrk.GetDisp() / 2. && mytrk.GetProb()>0.03 && mytrk.GetDisp() < 2) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.3&&mytrk.GetPtPrime()<0.4 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=4 + mytrk.GetDisp()*mytrk.GetDisp() / 3. && mytrk.GetProb()>0.03 && mytrk.GetDisp() < 3) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.4&&mytrk.GetPtPrime()<0.5 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=3 + mytrk.GetDisp()*mytrk.GetDisp() / 3. && mytrk.GetProb()>0.02) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.5&&mytrk.GetPtPrime()<0.7 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=2 + mytrk.GetDisp()*mytrk.GetDisp() / 8. && mytrk.GetProb()>0.01 && mytrk.GetDisp() < 4) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.7&&mytrk.GetPtPrime()<0.9 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 ) )
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>0||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>0||mytrk.GetHitCounter(3)<1)&&mytrk.GetMinsDphi(0)>0)||mytrk.GetGhost()<10) && mytrk.GetPtPrime()<0.7)
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>0||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>0||mytrk.GetHitCounter(3)<1)||mytrk.GetMinsDphi(0)>0)||mytrk.GetGhost()<10) && mytrk.GetPtPrime()>0.9&& mytrk.GetPtPrime()<0.9)
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>-1||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>-1||mytrk.GetHitCounter(3)<1)||mytrk.GetMinsDphi(0)>-1)||mytrk.GetGhost()<25) && mytrk.GetPtPrime()>0.9)
+          skip = true;
+                    
+        else if( skip )
+          event->RemoveTrackEntry(itrk);
+          //event->AddElecCand(&mytrk);
+          n_electrons--;
+          itrk--;
+          continue;
+      }
+      if(fill_inv_mass && event->GetCentrality()>=60)
+      {
+        bool skip = false; 
+        if (mytrk.GetPtPrime()>0.2&&mytrk.GetPtPrime()<0.3 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=3 + mytrk.GetDisp()*mytrk.GetDisp() / 3. && mytrk.GetProb()>0.02) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.3&&mytrk.GetPtPrime()<0.4 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 && mytrk.GetN0()>=2 + mytrk.GetDisp()*mytrk.GetDisp() / 8. && mytrk.GetProb()>0.01 && mytrk.GetDisp() < 4) )
+          skip = true;
+        else if (mytrk.GetPtPrime()>0.4&&mytrk.GetPtPrime()<0.6 && !(mytrk.GetEcore()/mytrk.GetPtot() > 0.8 ) )
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>0||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>0||mytrk.GetHitCounter(3)<1)&&mytrk.GetMinsDphi(0)>0)||mytrk.GetGhost()<10) && mytrk.GetPtPrime()<0.3)
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>0||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>0||mytrk.GetHitCounter(3)<1)||mytrk.GetMinsDphi(0)>0)||mytrk.GetGhost()<10) && mytrk.GetPtPrime()>0.3&& mytrk.GetPtPrime()<0.5)
+          skip = true;
+        if (!(((mytrk.GetMinsDphi(2)>-1||mytrk.GetHitCounter(2)<1)&&(mytrk.GetMinsDphi(3)>-1||mytrk.GetHitCounter(3)<1)||mytrk.GetMinsDphi(0)>-1)||mytrk.GetGhost()<25) && mytrk.GetPtPrime()>0.5)
+          skip = true;
+                    
+        else if( skip )
+          event->RemoveTrackEntry(itrk);
+          //event->AddElecCand(&mytrk);
+          n_electrons--;
+          itrk--;
+          continue;
+      }
+    }
 
     vtxhist->Fill(event->GetPreciseX(),event->GetPreciseY(),event->GetRunNumber());
     
@@ -148,7 +200,7 @@ void NewHitAssociation(const char* inFile0, const char* outFile, int par = 0)
     if (event->GetNtrack() < 2-use_only_vertex_hadrons)
       continue;
 
-    for (int itrk = 0; itrk <  event->GetNhadron(); itrk++)
+    for (int itrk = 0; itrk <  event->GetNhadron()*remove_hadron_hits; itrk++)
     {
       MyDileptonAnalysis::MyHadron *mytrk = event->GetHadronEntry(itrk);
       if (remove_hadron_hits && (mytrk->GetPtPrime() < pt_cut || TMath::Abs(mytrk->GetPC3SDPHI())>2 || TMath::Abs(mytrk->GetPC3SDZ())>2)) 
